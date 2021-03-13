b3b63db261d04ce0ad8a7d11f5c93278bf27eaf1
tkuriyama <taro.kuriyama@gmail.com>
2021-03-13 15:20:20 -0500

    Adding mapFst and mapSnd

--- src/Internal/Utils.elm
+++ src/Internal/Utils.elm
@@ -19,8 +19,17 @@ snd (_, b) = b
 snds : List (a, b) -> List b
 snds = List.map snd
 
+
 splitPairs : List (a, b) -> (List a, List b)
 splitPairs pairs = (fsts pairs, snds pairs)
+
+mapFst : (a -> c) -> List (a, b) -> List (c, b)
+mapFst f =
+    List.map (\(a, b) -> (f a, b))
+
+mapSnd : (b -> c) -> List (a, b) -> List (a, c)
+mapSnd f =
+    List.map (\(a, b) -> (a, f b))
 
 
 --------------------------------------------------------------------------------
@@ -31,3 +40,9 @@ fmtFloat dp f =
     let cfg = { usLocale | decimals = Exact dp }
     in FormatNumber.format cfg f
 
+
+--------------------------------------------------------------------------------
+-- Reshaping
+
+reshapeSeriesPairs : List(a, List(b, c)) -> List(b, List(a, c))
+reshapeSeriesPairs pairs = []
--- tests/TestInternalUtils.elm
+++ tests/TestInternalUtils.elm
@@ -33,3 +33,63 @@ testSplitPairs =
         [ test "splitPairs" <|
               \_ -> splitPairs [(1, 2), (3, 4)] |> Expect.equal ([1, 3], [2, 4])
         ]
+
+testMapFstSnd : Test
+testMapFstSnd =
+    let f n = n * 2
+        pairs = [(1, 2), (3, 4)]
+    in describe "Test mapFst and mapSnd"
+        [ test "mapFst" <|
+              \_ -> mapFst f pairs |> Expect.equal [(2, 2), (6, 4)]
+        , test "mapSnd" <|
+              \_ -> mapSnd f pairs |> Expect.equal [(1, 4), (3, 8)]
+        ]
+
+--------------------------------------------------------------------------------
+-- Formatting
+
+testFmtFloat : Test
+testFmtFloat =
+    describe "Test float formatting"
+        [ test "0 dp" <|
+              \_ -> fmtFloat 0 0.1234 |> Expect.equal "0"
+        , test "3 dp" <|
+              \_ -> fmtFloat 3 0.1234 |> Expect.equal "0.123"
+        ]
+
+
+--------------------------------------------------------------------------------
+-- Reshaping
+
+testReshapeSeriesPairs : Test
+testReshapeSeriesPairs =
+    let sp1 = [ ("Name1", [("Label1", 1.0), ("Label2", 2.0)])
+              ]
+        sp2 = [ ("Label1", [ ("Name1", 1.0) ])
+              , ("Label2", [ ("Name2", 2.0) ])
+              ]
+        sp3 = [ ("Name1", [ ("Label1", 1.0)
+                          , ("Label2", 2.0)
+                          ]
+                )
+              , ("Name2", [ ("Label1", 11.0)
+                          , ("Label2", 12.0)
+                          ]
+                )
+              ]
+        sp4 = [ ("Label1", [ ("Name1", 1.0)
+                           ,  ("Name1", 2.0)
+                           ]
+                )
+              , ("Label2", [ ("Name1", 2.0)
+                           , ("Name2", 12.0)
+                           ]
+                )
+              ]
+    in describe "Test reshape SeriesPairs with one name"
+        [ test "reshape sample" <|
+              \_ -> reshapeSeriesPairs sp1 |> Expect.equal sp2
+        ,
+            test "reshape sample with more than one name" <|
+                \_ -> reshapeSeriesPairs sp3 |> Expect.equal sp4
+        ]
